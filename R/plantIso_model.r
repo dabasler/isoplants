7c96b3365ec465e1e37faaa65d411298fbfc0400
dabasler <david.j.basler@gmail.com>
2023-05-23 15:12:46 +0200

    rebuild

--- R/plantIso_model.r
+++ R/plantIso_model.r
@@ -127,8 +127,8 @@ plant2H_model <- function(par, addpar=NULL, output = "all", verbose = FALSE){ # 
 #' @return a dataframe of calculated values (see details below)
 #' @keywords isotope, craig-gordon, leaf cellulose, leaf water
 #' @details
-#' @export
 #' Main code for the model, for detailed input parameter descriptions see ?plant18O_model() or ?plant2H_model() functions
+#' @export
 #'
 plantIso_model <- function(par, addpar=NULL, element="O", output = "all", verbose = FALSE) {
   # Define output names
